758283d6637dc1c362c33b4daf0bf407c1be3935
Daniel Schaffer <dan@dandoes.net>
2020-09-08 09:56:39 -0700

    fix: update paths and exports

--- lib/mocha/index.ts
+++ lib/mocha/index.ts
@@ -1,4 +1,1 @@
-import { mochaMarbles, MochaMarblesConfig } from './src/mocha-marbles'
-
-export { MochaMarblesConfig }
-module.exports = mochaMarbles
+export { mochaMarbles, MochaMarblesConfig } from './src/mocha-marbles'
